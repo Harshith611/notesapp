48349a722eccdf0a8a85ffc5f721b7632fae54ed
Harshith611 <120678082+Harshith611@users.noreply.github.com>
2024-03-07 12:00:37 +0530

    action file altered1

--- config/db.py
+++ config/db.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
 # Example db.py file
 # Use the service name defined in docker-compose.yml as the host
-mongo_host = "mongodb"
-mongo_port = 27017
-
-conn = MongoClient(f"mongodb://{mongo_host}:{mongo_port}/")
+mongo_uri="mongodb+srv://harshithkumar6510:harshithkumar6510@cluster0.uomdcii.mongodb.net/"
+conn=MongoClient(mongo_uri)
